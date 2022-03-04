d11619d954fa9eba6b98a0ea11e7cfa1ecf506e9
Damien MATHIEU <100044891+damien-mathieu1@users.noreply.github.com>
2022-03-04 18:12:52 +0100

    Debut du design
    Design de la barre de recherche effectué, à faire : faire le design de l'affichage des informations

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 ## Web application that'll fetch information about the weather in the city you will chose
 -Display the description of the weather
 -Display the temps also
+
+#content{
+  background-image: linear-gradient(to bottom, rgba(128, 255, 170, 0.5),rgba(0,0,128,0.5));
+  flex-basis : 100%;
+
+}
--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 
-var button = document.querySelector("button");
+var input = document.getElementById("input");
+
+input.addEventListener("keyup", function(event){
+  if (event.keyCode === 13) {
+      event.preventDefault();
+      document.getElementById("button").click();  
+  }
+});
+
 
 button.onclick= async function getWeatherInfo(){
-    ville= document.getElementById("city").value;
+    ville= document.getElementById("input").value;
     api_url = "http://api.openweathermap.org/data/2.5/weather?q="+ville+"&units=metric&lang=fr&appid=13a6b0a5726c35c06b87cc719ded487e";
     const response = await fetch(api_url);
     const data = await response.json();
-    console.log(data.weather[0].description);
-    document.getElementById('weather').textContent=data.weather[0].description;
+    console.log(data);
+    document.getElementById('ville').textContent=data.name+" : ";
+    document.getElementById('weather').textContent="Le temps est : "+data.weather[0].description;
     document.getElementById('temp').textContent=data.main.temp+" degrès";
+    document.getElementById("weather").scrollIntoView({
+      behavior: "smooth"
+  });
 
 }
 
